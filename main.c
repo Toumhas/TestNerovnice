8c2ae35bc14e17a1929811a46cae353851792e0e
toumhas <tomas.cihlar101@gmail.com>
2023-02-08 18:37:44 +0100

    Update1

--- main.c
+++ main.c
@@ -7,16 +7,19 @@ typedef struct{
     int pocet;
     float koeficient[20];
 }Tpolynom;
+//Struktura pro polynom, 20 míst pro koeficienty a počet koeficientů
 
 typedef struct{
     float a;
     float b;
 }Tinterval;
+//Intervaly ze kterých např. bisekce hledá výsledek
 
 typedef struct{
     int pocet;
     Tinterval interval[20];
 }TRadaIntervalu;
+//Seznam všech intervalů pro daný příklad
 
 float horner(Tpolynom * polynom, float bod)
 {
@@ -27,6 +30,7 @@ float horner(Tpolynom * polynom, float bod)
     }
     return vysledek;
 }
+//Hornerovo schéma, vraci výsledek polynomu v bodě
 
 int strVytahnoutCislaInt(char string[])
 {
@@ -58,19 +62,26 @@ float loadSoubor(char pathSoubor[], TRadaIntervalu * radaI, Tpolynom * polynom)
     fscanf(f, "%s\n%s%f\n", nadpis, pom, &presnost);
     fscanf(f, "%s", pom);
     polynom->pocet = strVytahnoutCislaInt(pom);
-    printf("%d\n", polynom->pocet);
     for(int i = 0; i<polynom->pocet; i++)
     {
         fscanf(f, "%f,", &polynom->koeficient[i]);
     }
-    printf("%f", polynom->koeficient[3]);
+    fscanf(f, "%s", pom);
+    polynom->pocet = strVytahnoutCislaInt(pom);
+    for(int i = 0; i<polynom->pocet; i++)
+    {
+        fscanf(f, "%f,", &polynom->koeficient[i]);
+    }
 }
 
 int main()
 {
     Tpolynom * polynom = malloc(sizeof(Tpolynom));
     TRadaIntervalu * radaI = malloc(sizeof(TRadaIntervalu));
-    float presnost = loadSoubor("input.txt", radaI, polynom);
+    printf("Zadejte jmeno souboru, ze ktereho chcete vzit priklad:\n");
+    char cesta[100];
+    scanf("%99s", cesta);
+    float presnost = loadSoubor(cesta, radaI, polynom);
     printf("\n%f", horner(polynom, 3));
     return 0;
 }
